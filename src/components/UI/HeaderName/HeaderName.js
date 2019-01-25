e50febfbf7b5380f6ac1c53043a44cd54ecfd9dc
sarru1291 <sarru1291@gmail.com>
2019-01-25 12:38:04 +0530

    header adding

---
+++ src/components/UI/HeaderName/HeaderName.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from './HeaderName.css'; 
+
+const HeaderName=(props)=>(
+    <div>
+      <h1 className={classes.HeaderName} > RetinoSkin</h1>
+    </div>
+  );
+
+export default HeaderName;
---
+++ src/containers/Header/Header.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+import HeaderName from "../../components/UI/HeaderName/HeaderName";
+
+class Header extends Component{
+  render(){
+    return(
+      <div>
+        <HeaderName/>
+      </div>
+    );
+  }
+}
+export default Header;
