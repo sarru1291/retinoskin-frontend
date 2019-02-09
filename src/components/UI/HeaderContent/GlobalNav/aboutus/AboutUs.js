e94eb84f93698e5468270eb6b11734f115c1406f
sarru1291 <sarru1291@gmail.com>
2019-02-09 15:53:05 +0530

    aboutus contact us

--- src/components/UI/HeaderContent/GlobalNav/aboutus/AboutUs.js
+++ src/components/UI/HeaderContent/GlobalNav/aboutus/AboutUs.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-
+import classes from './AboutUs.css';
 class AboutUs extends Component {
   render() {
     return (
-      <div>
-        Aboutus
+      <div className={classes.AboutUs}>
+        <h1>About us</h1>
+        <p>
+          Sarvottam Kumar<br></br>
+          BVCOE (New Delhi)
+        </p>
+        <p>
+          Saurav Ghosh<br></br>
+          BVCOE (New Delhi)
+        </p>
       </div>
     );
   }
--- src/components/UI/HeaderContent/GlobalNav/contactus/ContactUs.js
+++ src/components/UI/HeaderContent/GlobalNav/contactus/ContactUs.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import classes from './Contactus.css';
 class ContactUs extends Component {
   render() {
     return (
-      <div>
-        Contact us
+      <div className={classes.ContactUs}>
+        <h1>Contact Us:</h1>
+        <p>Team: Tebraa<br></br>
+          Phone no: +919958938566<br></br>
+          Email:sarru1291@gmail.com<br></br>
+        </p>
       </div>
     );
   }
